c2c71f08cb505a35a1550c16f4f95bd7241749ad
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-25 15:52:15 -0400

    A bit of refactoring with the top annotations. Created a post so the user could like another user, or they could be liked by another user.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,12 +7,16 @@ using AutoMapper;
 using DatingApp.Data;
 using DatingApp.DTOs;
 using DatingApp.Helpers;
+using DatingApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.Controllers
 {
-    [Authorize, Route("api/[controller]"), ApiController, ServiceFilter(typeof(LogUserActivity))]
+    [Authorize, 
+     Route("api/[controller]"), 
+     ApiController, 
+     ServiceFilter(typeof(LogUserActivity))]
     public class UsersController : ControllerBase
     {
         private readonly IDatingRepository _repo;
@@ -67,6 +71,34 @@ namespace DatingApp.Controllers
                 return NoContent();
             
             throw new Exception($"Updating user {id} failed on save.");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if (like != null)
+                return BadRequest("You already like this user.");
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if (await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user.");
         }
     }
 }
