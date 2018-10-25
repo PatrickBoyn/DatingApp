0e4ca6b6892dc4fb3398dde911beb41442d3be3d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-25 03:03:50 -0400

    Added the LIke class for the table.

--- Models/Like.cs
+++ Models/Like.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DatingApp.Models
 {
+    [Table("Likes")]
     public class Like
     {
-        
+        public int LikerId { get; set; }
+        public int LikeeId { get; set; }
+        public User Liker { get; set; }
+        public User Likee { get; set; }
     }
 }
