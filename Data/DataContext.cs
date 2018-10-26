36dd1e0f6e4795fa3e0f901f29de7f4ad36f6d6d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-26 18:13:46 -0400

    Configured Entity Framework so it would know how the messages would work.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,6 +9,7 @@ namespace DatingApp.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
         
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
@@ -21,6 +22,14 @@ namespace DatingApp.Data
             
             modelBuilder.Entity<Like>().HasOne(u => u.Liker).WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Sender).WithMany(m => m.MessagesSent).OnDelete(DeleteBehavior.Restrict);
+            
+            
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Recipient).WithMany(m => m.MessagesReceived).OnDelete(DeleteBehavior.Restrict);
+
         }
         
     }
