5c3e0c2e5fc5d477bf99dc88a65419ab3451e8dd
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-26 20:00:41 -0400

    Created the get message controller. Also started the Create Message controller.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,7 +1,49 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.Data;
+using DatingApp.Helpers;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DatingApp.Controllers
 {
-    public class MessagesController
+    
+    [Authorize, 
+     Route("api/users/{userId}/[controller]"), 
+     ApiController, 
+     ServiceFilter(typeof(LogUserActivity))]
+    public class MessagesController : ControllerBase
     {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IDatingRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok(messageFromRepo);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            
+        }
         
     }
 }
