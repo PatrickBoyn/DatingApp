278826e256a6f8efb11a3d3f06982c4ba60d5844
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-10-26 20:19:30 -0400

    This method creates a post. If it fails it throws an Exception.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.Data;
+using DatingApp.DTOs;
 using DatingApp.Helpers;
 using DatingApp.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -42,7 +44,25 @@ namespace DatingApp.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageForCreationDto.SenderId = userId;
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+
+            if (recipient == null)
+                return BadRequest("Could not find user.");
+
+            var message = _mapper.Map<Message>(messageForCreationDto);
             
+            _repo.Add(message);
+
+            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+            
+            if (await _repo.SaveAll())
+                return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
+            
+            throw new Exception("Failed to save created message.");
         }
         
     }
